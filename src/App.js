d333ec7a65c26232049ff6c2e773942014c97b83
Ralf Braitling <work@braitling.de>
2021-04-07 07:00:11 +0200

    docs try out
    remove test text and try out an extended commit
    
    test footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css'
 class BooksApp extends React.Component {
   state = {
     /**
-     * test
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
