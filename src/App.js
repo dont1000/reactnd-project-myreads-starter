7a15a24a9d624861dee728c14f2b544136ade3bd
Ralf Braitling <work@braitling.de>
2021-04-08 16:57:08 +0200

    add API call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
- // import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchPage from './SearchPage'
 import BookrackPage from './BookrackPage'
 import {Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
-
+  state = {books:[]};
+  componentDidMount() {
+    BooksAPI.getAll().then((allBooks) => {
+      this.setState(() => {
+        return { books: allBooks };
+      });
+    });
+  }
 
   render() {
     return (
