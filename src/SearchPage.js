c4fc75ce3421b90a3573193d7ab40401061ee7f3
Ralf Braitling <work@braitling.de>
2021-04-12 13:56:38 +0200

    fix: books are shown twice on the resultpage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -8,7 +8,7 @@ class SearchPage extends React.Component {
   state = {
     searchedBooks: [],
     msg: "",
-    isLoading: false,
+    isLoading:false,
   };
   noBookMsg = "No books found, please try again";
 
@@ -18,62 +18,58 @@ class SearchPage extends React.Component {
 
   reset = () => {
     this.setState({ searchedBooks: [], isLoading: true });
-  };
+  }
 
-  search = (query) => {
+  searchBook = (query) => {
+   
     BooksAPI.search(query).then((resultBooks) => {
       if (!resultBooks.length) {
-        this.setState({
-          msg: this.noBookMsg,
-          isLoading: false,
+        this.setState({ 
+          msg: this.noBookMsg ,
+          isLoading:false
         });
         return;
       }
-      const searchResultIds = resultBooks.map((book) => book.id);
-      const searchedBooksinList = this.props.allBooks.filter((book) => {
-        return searchResultIds.includes(book.id);
+   
+      const allBookIds = this.props.allBooks.map((book) => book.id);
+      resultBooks.forEach(book => {
+        if (allBookIds.includes(book.id)){
+          const indexAllbooks = allBookIds.indexOf(book.id)
+          book.shelf = this.props.allBooks[indexAllbooks].shelf;
+        }else{
+          book.shelf='none'
+        }
       });
+
       this.setState({
-        searchedBooks: [...resultBooks, ...searchedBooksinList],
-        isLoading: false,
+        searchedBooks: resultBooks,
+        isLoading:false
       });
     });
   };
 
-  searchBook = (query) => {
+  search=(query) =>{
     this.reset();
-    this.search(query);
-  };
+    this.searchBook(query);
+  }
 
   render() {
-    return (
-      <div className="search-books">
+    return <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            <Searchbar
-              handleTextInput={this.searchBook}
-              handleChange={this.emptyMessage}
-            />
+            <Searchbar handleTextInput={this.search} handleChange={this.emptyMessage} />
           </div>
         </div>
         <div className="search-books-msg">{this.state.msg}</div>
-        {this.state.isLoading && (
-          <div class="load5">
-            <div class="loader">Loading...</div>
-          </div>
-        )}
+        {this.state.isLoading && <div class="load5"><div class="loader">Loading...</div></div>}
         <div className="search-books-results">
-          <ListBooks
-            books={this.state.searchedBooks}
-            updateBooks={this.props.updateBooks}
-          />
+          <ListBooks books={this.state.searchedBooks} updateBooks={this.props.updateBooks} />
         </div>
         <div> {this.state.errorMsg} </div>
-      </div>
-    );
+      </div>;
   }
 }
 
