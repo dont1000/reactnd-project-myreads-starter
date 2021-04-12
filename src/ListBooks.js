4647e31caeb5b2357b67fe607793340c68b8c597
Ralf Braitling <work@braitling.de>
2021-04-12 13:49:03 +0200

    fix: error for duplicated key

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -5,7 +5,7 @@ const ListBooks =(props) =>{
 const {books, updateBooks} = props
  return  <ol className="books-grid">
        {books ? books.map((book) => {
-         return <li key={book.title}>
+         return <li key={book.id}>
              <Book book={book} updateBooks= {updateBooks}/>
            </li>;
        }):''}
